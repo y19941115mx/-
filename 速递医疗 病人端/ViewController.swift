0969eda4da5864b23a865878fd06ccfdccf6b182
y19941115mx <1960892068@qq.com>
2017-10-29 21:47:18 +0800

    初始化项目

---
+++ 速递医疗 病人端/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  速递医疗 病人端
+//
+//  Created by admin on 2017/10/29.
+//  Copyright © 2017年 victor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
