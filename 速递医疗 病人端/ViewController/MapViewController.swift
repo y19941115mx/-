659d80d4bd40d620a1054b1ee2f1b4d21c37bac3
victor <1960892068@qq.com>
2018-03-15 10:46:42 +0800

    提交代码

--- 速递医疗 病人端/ViewController/MapViewController.swift
+++ 速递医疗 病人端/ViewController/MapViewController.swift
@@ -7,18 +7,21 @@
 //
 
 import UIKit
+import SnapKit
 
 class MapViewController: BaseViewController,MAMapViewDelegate {
     var mapView:MAMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 添加默认样式
+        initView()
         // 获取医生地图模式列表
         NetWorkUtil<DoctorListBean>.init(method: .mapdoctors(APPLICATION.lon, APPLICATION.lat)).newRequest(successhandler: { (bean, json) in
             
             AMapServices.shared().enableHTTPS = true
             self.mapView = MAMapView(frame: self.view.bounds)
             self.mapView.delegate = self
-            self.mapView.isShowsUserLocation = true
+            self.mapView.showsUserLocation = true
             self.mapView.userTrackingMode = .follow
             // 绘制医生位置
             self.addDoctorPoint(data: bean.doctorDataList)
@@ -28,6 +31,18 @@ class MapViewController: BaseViewController,MAMapViewDelegate {
         }
         
         // Do any additional setup after loading the view.
+    }
+    
+    private func initView() {
+        // 添加一行提示
+        let label = UILabel()
+        label.text = "请检查GPS设置"
+        label.textColor = UIColor.lightGray
+        self.view.addSubview(label)
+        label.snp.makeConstraints { (make) in
+            make.center.equalToSuperview()
+        }
+        
     }
     
     private func addDoctorPoint(data:[DoctorBean]?) {
